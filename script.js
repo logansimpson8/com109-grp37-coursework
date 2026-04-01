b1be73bde9ca536234087f8cfee9a8d598d08d34
Logan Simpson <logansimpson136@gmail.com>
2026-04-01 19:02:20 +0100

    added light mode

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+// form submission box logic
+
 const form = document.querySelector('.contact-form');
 const overlay = document.getElementById('successform');
 const closeBtn = document.getElementById('closeformBtn');
@@ -15,4 +19,31 @@ closeBtn.addEventListener('click', function() {
         top: 0,
         behavior: 'smooth'
     });
+});
+
+// light mode toggle logic
+
+const themeToggleBtn = document.getElementById('themeToggle');
+const body = document.body;
+const currentTheme = localStorage.getItem('theme');
+
+// the pre check for whatever user had last time
+if (currentTheme === 'light') {
+    body.classList.add('light-mode');
+    themeToggleBtn.textContent = 'Dark Mode';
+} else {
+    themeToggleBtn.textContent = 'Light Mode';
+}
+
+// the manual switch logic
+themeToggleBtn.addEventListener('click', function() {
+
+    body.classList.toggle('light-mode');
+    if (body.classList.contains('light-mode')) {
+        localStorage.setItem('theme', 'light'); 
+        themeToggleBtn.textContent = 'Dark Mode'; 
+    } else {
+        localStorage.setItem('theme', 'dark'); 
+        themeToggleBtn.textContent = 'Light Mode'; 
+    }
 });
